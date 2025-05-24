c43cbf797239ac181c2711b657390462a412151f
thiago0-08 <housezx210@gmail.com>
2025-05-24 20:08:31 -0300

    criando carrinho

--- src/Components/Carrinho/Carrinho.jsx
+++ src/Components/Carrinho/Carrinho.jsx
@@ -1,19 +1,38 @@
-
 import React from 'react'
 import Navbar from '../NavBar/Navbar'
 import './carrinho.css'
 import { useNavigate } from 'react-router-dom'
 
 const Carrinho = () => {
-  return (
-    <div>
-      <Navbar />
-        <div className='container'>
-            <h1>Carrinho</h1>
-            <p>Adicione produtos ao carrinho e finalize sua compra.</p>
+    return (
+        <div>
+            <Navbar />
+            <div className='container'>
+                <h1>Carrinho</h1>
+                <p>Adicione produtos ao carrinho e finalize sua compra.</p>
+                <div className='carrinho'>
+                    <div className='carrinho-produto'>
+                        <img src="src\assets\produto1.jpg" alt="Produto" />
+                        <h2>Nome do Produto</h2>
+                        <p>Descrição do produto</p>
+                        <p>Preço: R$ 100,00</p>
+                        <button>Remover do Carrinho</button>
+                    </div>
+                    <div className='carrinho-produto'>
+                        <img src="src\assets\produto1.jpg" alt="Produto" />
+                        <h2>Nome do Produto</h2>
+                        <p>Descrição do produto</p>
+                        <p>Preço: R$ 100,00</p>
+                        <button>Remover do Carrinho</button>
+                    </div>
+            </div>
         </div>
-    </div>
-  )
+            <div className='carrinho-total'>
+                <h2>Total: R$ 200,00</h2>
+                <button>Finalizar Compra</button>
+            </div>
+        </div>
+    )
 }
 
 export default Carrinho
